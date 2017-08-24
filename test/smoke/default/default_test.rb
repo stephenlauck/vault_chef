c3a60569ec192eeb6a5fd7ed422b4e1bc2b2b5de
Stephen Lauck <lauck@chef.io>
2017-08-24 18:02:08 -0400

    Add inspec tests for basic smoke test on Vault cluster

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,22 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+
+describe user('vault'), :skip do
+  it { should exist }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe group('vault'), :skip do
+  it { should exist }
+end
+
+describe http('http://10.0.10.10:8200/v1/sys/health') do
+  its('status') { should cmp 501 }
+  its('body') { should match '"initialized":false.*' }
+end
+
+describe systemd_service('vault') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
